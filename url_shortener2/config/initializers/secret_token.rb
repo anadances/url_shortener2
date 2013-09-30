773894af002c8ed9e96cd3adfee8ac264d53093e
Ana Tighe & Abby Ihrig <student@epicodus.com>
2013-09-30 10:02:49 -0700

    URL shortener

---
+++ url_shortener2/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+UrlShortener2::Application.config.secret_key_base = '88e9a2995eba8367f8ee22413feffa2ac337c0262fbb0433c1a88b04a9f948fec4d21b10572126ae605e7f7a24c3c5fb7cf3393a71b71df511289b49c0aa7723'
